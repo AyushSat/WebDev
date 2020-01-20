0b84d1be6410558f11319c1751c031f1e8df7bcc
AyushSat <themessimess@gmail.com>
2020-01-19 19:48:13 -0800

    initcommit

---
+++ Home/script.js
@@ -0,0 +1,10 @@
+function airlineclick() {
+	document.querySelector(".header").setAttribute("id", "homepageout");
+	document.querySelector(".airlineinfo").setAttribute("id", "airlineinfoin");
+
+}
+
+function homeclick(){
+	document.querySelector(".header").setAttribute("id","homepagein");
+	document.querySelector(".airlineinfo").setAttribute("id", "airlineinfoout");
+}
