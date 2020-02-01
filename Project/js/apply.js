6795dc7eb5abba376aef4b58c6a6cecc9b23c68b
AyushSat <themessimess@gmail.com>
2020-02-01 12:43:24 -0800

    thankscreen

--- Project/js/apply.js
+++ Project/js/apply.js
@@ -1,3 +1,16 @@
 function animatein(){
-    document.querySelector(".thanks").setAttribute("id", "thanksin");
+
+    const inputs = document.querySelectorAll("input");
+    let valid = true;
+    for(let i =0;i<inputs.length;i++){
+        console.log(inputs[i].checkValidity());
+        if(!inputs[i].checkValidity()){
+            valid = false;
+        }
+    }
+    console.log(valid);
+    if(valid){
+        console.log("testmonky");
+        document.querySelector(".thanks").setAttribute("id", "thanksin");
+    }
 }
