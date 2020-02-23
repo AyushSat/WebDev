418c91905613e803bc77fc2bcc03852c36772268
AyushSat <themessimess@gmail.com>
2020-02-23 13:30:57 -0800

    appoly

---
+++ Project/js/apply.js
@@ -0,0 +1,23 @@
+ let currNum = 1;
+
+function b1click(){
+    currNum = 1;
+    
+}
+
+function b2click(){
+    currNum = 2;
+}
+
+function b3click(){
+    currNum = 3;
+}
+
+
+ if(currNum == 1){
+    document.querySelector("#first").
+ }else if(currNum ==2){
+
+ }else if(currNum ==3){
+
+ }
